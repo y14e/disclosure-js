e7de7058a799b8c02592e2dcaec4adcc2c6bff32
Yusuke Kamiyamane <y@y14e.com>
2025-04-14 00:07:25 +0900

    20250414

--- disclosure-css.ts
+++ disclosure-css.ts
@@ -51,9 +51,7 @@ export class Disclosure {
       return;
     }
     event.preventDefault();
-    const focusables = [...this.summaryElements].filter(summary => {
-      return this.isFocusable(summary.parentElement!);
-    });
+    const focusables = [...this.summaryElements].filter(summary => this.isFocusable(summary.parentElement!));
     const currentIndex = focusables.indexOf(document.activeElement as HTMLElement);
     const length = focusables.length;
     let newIndex = 0;
