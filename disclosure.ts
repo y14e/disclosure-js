8cd4847675024e0e768275ef53ea8e373b4f2e83
Yusuke Kamiyamane <y@y14e.com>
2025-06-14 23:44:02 +0900

    20250614

--- disclosure.ts
+++ disclosure.ts
@@ -26,10 +26,7 @@ export class Disclosure {
       },
     };
     this.settings = {
-      animation: {
-        ...this.defaults.animation,
-        ...options?.animation,
-      },
+      animation: { ...this.defaults.animation, ...options?.animation },
     };
     if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
       this.settings.animation.duration = 0;
